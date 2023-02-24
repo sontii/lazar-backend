45996ca1f21d3f7db70537cd496d70773ce6e7db
Fridrich Ferenc <sontii78@gmail.com>
2023-02-24 18:58:23 +0100

    .

--- authServer.js
+++ authServer.js
@@ -18,7 +18,6 @@ app.use(cors())
 
 app.use(express.json())
 
-let refreshTokens = []
 
 app.get("/api", async (req, res) => {
     res.json({ status: "AUTH server is running and ready to serv" })
@@ -26,20 +25,35 @@ app.get("/api", async (req, res) => {
 
 
 //generate new access token from valid refresh token
-app.post("/api/token", (req, res) => {
+app.post("/api/auth/token", (req, res) => {
 	const refreshToken = req.body.token
 	if (refreshToken == null) return res.sendStatus(401)
-	if (!refreshTokens.includes(refreshToken)) return res.sendStatus(403)
 	jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET, (err, user) => {
 		if (err) return res.sendStatus(403)
-		const accessToken = generateAccessToken({ name: user.name })
+		const accessToken = generateAccessToken({ user: req.body.email })
 		res.json({ accessToken: accessToken })
 	})
 })
 
 
-app.post("/api/register", async (req, res) => {
-	// Authenticate User
+app.post("/api/auth/register", async (req, res) => {
+	//check if user already exist
+	try {
+		// "?" in query for sanitaze query params
+		const query = `SELECT user FROM users WHERE user = ?`
+		const [queryResult] = await (await connection).query(query, [req.body.email])
+		// [param?] to "?" in query params
+
+		//if no user
+		if (queryResult[0]) {
+			return res.status(403).send({ msg: "Already exist" })
+		}
+
+	} catch (err) {
+		res.status(500).send({ msg: "Server error" })
+	}
+
+	// Register User
 	try {
 		//create hashed password from body password
 		const hashedPassword = await bcrypt.hash(req.body.password, 10)
@@ -47,63 +61,60 @@ app.post("/api/register", async (req, res) => {
 		const query = `INSERT INTO users (user, password) VALUES (?, ?)`
 		const [rows] = await (
 			await connection
-		).query(query, [req.body.user, hashedPassword])
+		).query(query, [req.body.email, hashedPassword])
 		// [param?, param?] to "?" in query params
 
 		res.status(201).send()
 	} catch (err) {
 		res.status(500).send(err.message)
-	} 
+	}
 })
 
-app.post("/api/login", async (req, res) => {
+app.post("/api/auth/login", async (req, res) => {
 	// Authenticate User
 	const postData = req.body
 	const user = {
 		email: postData.email,
 		password: postData.password,
 	}
-	
-	
-    try {
+
+	try {
 		// "?" in query for sanitaze query params
-		const query = `SELECT user, password FROM users WHERE user = ?`
+		const query = `SELECT user, password, isadmin FROM users WHERE user = ?`
 		const [queryResult] = await (await connection).query(query, [user.email])
 		// [param?] to "?" in query params
 
 		//if no user
 		if (!queryResult[0]) {
-			return res.status(404).send({msg: 'Cannot find user'})
+			return res.status(404).send({ msg: "Cannot find user" })
 		}
-		
-		//compare hashed password
-		try{
-			if(await bcrypt.compare(user.password, queryResult[0].password)){
 
+		const isAdmin = queryResult[0].isadmin
+
+		try {
+			//compare hashed password
+			if (await bcrypt.compare(user.password, queryResult[0].password)) {
 				const accessToken = generateAccessToken(user)
 
 				const refreshToken = jwt.sign(user, process.env.REFRESH_TOKEN_SECRET, {
 					expiresIn: process.env.JWT_REFRESH_EXPIRATION,
 				})
-                refreshTokens.push(refreshToken)
-
 
 				res.status(201).send({
 					email: req.body.email,
 					accessToken: accessToken,
 					refreshToken: refreshToken,
+					isadmin: isAdmin
 				})
 			} else {
 				res.status(404).send("Not allowed")
 			}
-		} catch(err) {
-			res.status(500).send({msg: "Server error"})
+		} catch (err) {
+			res.status(500).send({ msg: "Server error" })
 		}
-
-	} catch(err) {
-		res.status(500).send({msg: "Server error"})
+	} catch (err) {
+		res.status(500).send({ msg: "Server error" })
 	}
-
 })
 
 function generateAccessToken(user) {
@@ -112,4 +123,4 @@ function generateAccessToken(user) {
 	})
 }
 
-app.listen(port)
+app.listen(port, '0.0.0.0')
