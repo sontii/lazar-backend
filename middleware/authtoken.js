528d17a6602eac4b4d8a9ef42fd183b9d958f9e7
Fridrich Ferenc <sontii78@gmail.com>
2023-02-25 15:30:48 +0100

    .

--- middleware/authtoken.js
+++ middleware/authtoken.js
@@ -7,7 +7,7 @@ const authenticateToken = (req, res, next) => {
 
 	jwt.verify(token, process.env.ACCES_TOKEN_SECRET, (err, user) => {
 		console.log(err)
-		if (err) return res.sendStatus(403)
+		if (err) return res.sendStatus(403).json({msg: err})
 		req.user = user
 		next()
 	})
--- routes/blokk.js
+++ routes/blokk.js
@@ -8,23 +8,6 @@ router.get('/', (req, res) => {
 	res.send('API is running')
 });
 
-
-/* const authenticateToken = (req, res, next) => {
-	const authHeader = req.headers["authorization"]
-	const token = authHeader && authHeader.split(" ")[1]
-	if (token == null) return res.sendStatus(401)
-
-	jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
-		console.log(err)
-		if (err) return res.sendStatus(403)
-		req.user = user
-		next()
-	})
-}
- */
 router.get('/:start-:end', authenticateToken, blokk.blokkRange)
-
-
-
 
 module.exports = router
