f34ca17fa2b00c87c747d4286caffc9731cf0a8a
Fridrich Ferenc <sontii78@gmail.com>
2023-04-13 21:53:40 +0200

    .

--- queries/cikkCsoportQuery.js
+++ queries/cikkCsoportQuery.js
@@ -9,7 +9,12 @@ exports.cikkCsoport = async (req, res) => {
 
 	try {
 		// '?' in query for sanitaze query params
-		const query = `SELECT * FROM cikk JOIN cikk_csoport ON cikk.arukod=cikk_csoport.arukod_id JOIN nomenklatura ON cikk_csoport.nomen_id=nomenklatura.id WHERE arukod = 3509677`
+		const query = `SELECT * FROM blokk
+						JOIN cikk ON blokk.arukod_id=cikk.cikk_kod
+						JOIN cikk_csoport ON cikk.cikk_id=cikk_csoport.arukod_id
+						JOIN nomenklatura ON cikk_csoport.nomen_id=nomenklatura.id
+						WHERE blokk.datum BETWEEN "2023-03-01" AND "2023-03-01" AND nomenklatura.kod = 2040401020
+						LIMIT 25;`
 
 		// [start end] to '?' in query params
 		const [rows] = await pool.query(query, [start, end])
