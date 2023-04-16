960f56fe711da9c52673df482c43d295ea2038e5
Fridrich Ferenc <sontii78@gmail.com>
2023-04-16 19:58:36 +0200

    .

--- queries/nomenklaturaQuery.js
+++ queries/nomenklaturaQuery.js
@@ -17,7 +17,7 @@ exports.nomenklatura = async (req, res) => {
 		const mutateRow = []
 		mutateRow.push({
 			name: "",
-			children: [],
+			children: [1372174, 1372175, 1372176],
 			id: 0,
 			parent: null,
 		})
