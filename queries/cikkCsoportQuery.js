c9e2bb5fe509aed17203e178caaa09ec59ef4a55
sontii <sontii78@gmail.com>
2023-05-23 14:02:55 +0200

    csikkcs

--- queries/cikkCsoportQuery.js
+++ queries/cikkCsoportQuery.js
@@ -4,6 +4,7 @@ const pool = require('../config/db')
 exports.cikkCsoport = async (req, res) => {
 	const { start } = req.params
 	const { end } = req.params
+	const {csoport} = req.params
 
 	try {
 		// '?' in query for sanitaze query params
@@ -11,11 +12,11 @@ exports.cikkCsoport = async (req, res) => {
 						JOIN cikk ON blokk.arukod_id=cikk.cikk_kod
 						JOIN cikk_csoport ON cikk.cikk_id=cikk_csoport.arukod_id
 						JOIN nomenklatura ON cikk_csoport.nomen_id=nomenklatura.id
-						WHERE blokk.datum BETWEEN "2023-03-01" AND "2023-03-01" AND nomenklatura.kod = 2040401020
+						WHERE blokk.datum BETWEEN ? AND ? AND nomenklatura.kod = ?
 						LIMIT 25;`
 
 		// [start end] to '?' in query params
-		const [rows] = await pool.query(query, [start, end])
+		const [rows] = await pool.query(query, [start, end, csoport])
 
 		if (!rows[0]) {
 			return res.status(404).json({ msg: `Couldn't find data` })
